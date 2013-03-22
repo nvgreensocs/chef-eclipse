cbe89f82d58f6e7eb81fc59f7fac775ee691abb2
Jérome Da Costa <jerome@greensocs.com>
2013-03-22 14:28:55 +0100

    Changed download url for eclipse and eclipse installation is now under #{node[:prefix]}/eclipse

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,27 +11,25 @@
 # limitations that may apply.
 # 
 # ENDLICENSETEXT
+base_directory = "#{node[:prefix]}"
+eclipse_directory = "#{base_directory}/eclipse"
+user_home = "/#{node[:user]}"
 
 bash "Installing Eclipse..." do
     code <<-EOH
-    if [ ! -e /usr/eclipse ]; then
+    if [ ! -e "#{eclipse_directory}" ]; then
         # Depending of the running guest...
         processor=`uname -p`
-        cd /tmp
+        cd "#{base_directory}" 
         if [ "$processor" = "x86_64" ]; then
-            wget http://greensocs.com/files/eclipse/eclipse-cpp-juno-SR2-linux-gtk-x86_64.tar.gz -O eclipse.tar.gz
+             wget http://ftp.osuosl.org/pub/eclipse//technology/epp/downloads/release/juno/SR2/eclipse-cpp-juno-SR2-linux-gtk-x86_64.tar.gz -O eclipse.tar.gz
         else
-            wget http://greensocs.com/files/eclipse/eclipse-cpp-juno-SR2-linux-gtk.tar.gz -O eclipse.tar.gz
+            wget http://ftp.ussg.iu.edu/eclipse/technology/epp/downloads/release/juno/SR2/eclipse-cpp-juno-SR2-linux-gtk.tar.gz -O eclipse.tar.gz
         fi
-        wget http://greensocs.com/files/eclipse/gdb_hardware.tar.bz2
-        cd /usr
-        tar -xf /tmp/eclipse.tar.gz
-        cd eclipse
-        tar -xf /tmp/gdb_hardware.tar.bz2
-        rm /usr/bin/eclipse
-        ln -s /usr/eclipse/eclipse /usr/bin/eclipse
-        rm /tmp/eclipse.tar.gz
-        rm /tmp/gdb_hardware.tar.bz2
+        tar -xf eclipse.tar.gz && rm eclipse.tar.gz &&
+        cd #{eclipse_directory} &&
+        wget http://greensocs.com/files/eclipse/gdb_hardware.tar.bz2 &&
+        tar -xf gdb_hardware.tar.bz2 && rm gdb_hardware.tar.bz2
     else
         echo "Already installed."
     fi
