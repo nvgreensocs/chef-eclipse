8f90569f83d58173d42c00af9b9d686c049bcdc3
Mark Burton <mark@greensocs.com>
2013-03-26 20:29:21 +0100

    Better handling of eclipse install directory

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,27 +11,40 @@
 # limitations that may apply.
 # 
 # ENDLICENSETEXT
-base_directory = "#{node[:prefix]}"
-eclipse_directory = "#{base_directory}/eclipse"
-user_home = "/#{node[:user]}"
 
 bash "Installing Eclipse..." do
     code <<-EOH
-    if [ ! -e "#{eclipse_directory}" ]; then
+
+    if [ -w /usr/local/share ]; then
+      prefix=/usr/local/share/
+   else
+      prefix="#{node[:prefix]}/"
+   fi
+
+    if [ ! -e $prefix/eclipse ]; then
         # Depending of the running guest...
         processor=`uname -p`
-        cd "#{base_directory}" 
+        cd $prefix
+
         if [ "$processor" = "x86_64" ]; then
              wget http://ftp.osuosl.org/pub/eclipse//technology/epp/downloads/release/juno/SR2/eclipse-cpp-juno-SR2-linux-gtk-x86_64.tar.gz -O eclipse.tar.gz
         else
             wget http://ftp.ussg.iu.edu/eclipse/technology/epp/downloads/release/juno/SR2/eclipse-cpp-juno-SR2-linux-gtk.tar.gz -O eclipse.tar.gz
         fi
         tar -xf eclipse.tar.gz && rm eclipse.tar.gz &&
-        cd #{eclipse_directory} &&
+        cd $prefix/eclipse &&
         wget http://greensocs.com/files/eclipse/gdb_hardware.tar.bz2 &&
         tar -xf gdb_hardware.tar.bz2 && rm gdb_hardware.tar.bz2
+
+        if [ -w /usr/bin ]; then
+           if [ ! -e /usr/bin/eclipse ]; then 
+               ln -s $prefix/eclipse/eclipse /usr/bin/eclipse
+           fi
+        else
+           echo "export PATH=\\\$PATH:/$prefix/eclipse/" > bash.profile.d/eclipse.profile
+        fi
     else
-        echo "Already installed."
+        echo "Eclipse already installed."
     fi
     EOH
 end
